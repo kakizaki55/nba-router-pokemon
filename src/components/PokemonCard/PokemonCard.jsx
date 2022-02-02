13c71fe2e4603adbd18239895f9a2d8e58925dec
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-02 15:44:15 -0800

    just a bit of styleing

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -5,16 +5,14 @@ import style from './PokemonCard.css';
 export default function PokemonCard({ pokemon }) {
   const history = useHistory();
   const { name, weight, types, id, sprites } = pokemon;
-  console.log(sprites?.back_default);
+  console.log(sprites);
 
   const arrayOfSprites = [];
-
   const myFirstRecusion = (sprites) => {
     for (const key in sprites) {
       if (typeof sprites[key] === 'object') {
         myFirstRecusion(sprites[key]);
       } else {
-        console.log(sprites[key]);
         arrayOfSprites.push(sprites[key]);
       }
     }
@@ -27,12 +25,13 @@ export default function PokemonCard({ pokemon }) {
       <h1>{name}</h1>
       <h3>{weight} lbs</h3>
       {arrayOfSprites?.map((sprite_url) => (
-        <img src={`${sprite_url}`} className={style.img} />
+        <img src={`${sprite_url}`} className={style.img} key={sprite_url} />
       ))}
       {types?.map(({ type: { name } }) => (
         <h4 key={`${name}`}>{name}</h4>
       ))}
       <h5
+        className={style.button}
         onClick={() => {
           history.push('/');
         }}
