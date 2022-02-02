2c27c1c85d7a1df291a115790b5e57fbee47eef7
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-01 20:58:52 -0800

    just finshied up witht hehuse histry portion

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 export default function PokemonCard({ pokemon }) {
+  const history = useHistory();
+
   const { name, weight, types, id } = pokemon;
 
   return (
     <div>
-      {types ? (
-        types.map(({ type: { name } }) => <h1 key={`${name}`}>{name}</h1>)
-      ) : (
-        <></>
-      )}
-
+      {types?.map(({ type: { name } }) => (
+        <h1 key={`${name}`}>{name}</h1>
+      ))}
       <h1>{name}</h1>
-      <h1>{weight}lbs</h1>
+      <h1>{weight} lbs</h1>
+      <h5
+        onClick={() => {
+          history.push('/');
+        }}
+      >
+        back to home page
+      </h5>
     </div>
   );
 }
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -29,7 +29,6 @@ export default function Home() {
     };
     fetchData();
   }, []);
-  console.log(pokidex);
 
   return (
     <>
