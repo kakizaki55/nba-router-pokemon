0879b1d754c2eca1414756dac6c3e58d0176088c
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-03 17:59:46 -0800

    got two test passing but i need to wrapp in and act

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,7 @@
 import App from './App.jsx';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter, Route } from 'react-router';
 
 test('making sure the header renders and a list of pokemon who up ion loading', async () => {
   render(
@@ -26,6 +26,24 @@ test('making sure the pokemon details shows up when clicking a thumb nail', asyn
   const type = await screen.findByRole('heading', { name: /grass/i });
   const lbs = await screen.findByRole('heading', { name: /69 lbs/i });
   const images = screen.getAllByRole('img');
-  screen.debug(images);
+  expect(type).toBeInTheDocument();
+  expect(lbs).toBeInTheDocument();
   expect(images).toHaveLength(76);
+});
+
+test.only('making sure the item menu', async () => {
+  render(
+    <MemoryRouter initialEntries={['/items']}>
+      <Route path="/items">
+        <App></App>
+      </Route>
+    </MemoryRouter>
+  );
+  const pokemonthumb = await screen.findByText(/bulbasaur/i);
+  const itemButton = screen.getByText(/items/i);
+  userEvent.click(itemButton);
+  const masterball = await screen.findByText(/master/i);
+  const diveball = await screen.findByText(/dive/i);
+  expect(masterball).toBeInTheDocument();
+  expect(diveball).toBeInTheDocument();
 });
